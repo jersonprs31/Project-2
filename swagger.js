7aedaf1775da50dd0c125a1186f15e5c4978a949
jporrasr97 <jporrasr@miumg.edu.gt>
2026-09-19 15:41:37 -0600

    push

--- swagger.js
+++ swagger.js
@@ -5,12 +5,11 @@ const doc = {
     title: 'Garage Tracker API',
     description: 'API to manage vehicles and maintenance logs',
   },
-  host: 'localhost:8080', // Cambia esto por tu URL de Render cuando despliegues
-  schemes: ['http', 'https'],
+  host: 'garage-tracker-api.onrender.com',
+  schemes: ['https', 'http'],
 };
 
 const outputFile = './swagger.json';
 const routes = ['./server.js'];
 
-// Genera swagger.json
 swaggerAutogen(outputFile, routes, doc);
